67e44607f4b71af1a407ebccc3c6e7a12af84e2a
Koushik Roy <jc.koushikroy@gmail.com>
2026-07-17 19:33:45 +0530

    disable running tests

--- src/test/java/com/koushik/audiogpt/AudiogptApplicationTests.java
+++ src/test/java/com/koushik/audiogpt/AudiogptApplicationTests.java
@@ -1,13 +1,22 @@
 package com.koushik.audiogpt;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AudiogptApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
 
 }
