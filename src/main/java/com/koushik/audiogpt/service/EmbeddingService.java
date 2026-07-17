35daa71a3497e72f92be06bbcbcd5765b66e975a
Koushik Roy <jc.koushikroy@gmail.com>
2026-07-17 19:22:08 +0530

    updated application properties

--- src/main/java/com/koushik/audiogpt/service/EmbeddingService.java
+++ src/main/java/com/koushik/audiogpt/service/EmbeddingService.java
@@ -7,7 +7,6 @@ import org.springframework.ai.embedding.EmbeddingRequest;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
